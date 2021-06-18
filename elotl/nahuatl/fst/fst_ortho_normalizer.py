6334887b4f423890b71a6e5e13b6fcee7c60dd0f
Robert Pugh <robert.pugh408@gmail.com>
2021-06-17 20:40:24 -0700

    more

---
+++ elotl/nahuatl/fst/fst_ortho_normalizer.py
@@ -0,0 +1,23 @@
+from typing import List, Callable
+from pathlib import Path
+from string import punctuation
+import attapply
+
+
+class Normalizador(object):
+    def __init__(self, ortografia, tokenize: Callable = lambda s: s.split()):
+        path_to_hfst = Path("att", f"{ortografia}.att")
+        self.tokenize = tokenize
+        self.fst = attapply.ATTFST(path_to_hfst)
+
+    def norm(self, s):
+        secuencia_normalizada = [self.norm_palabra(p) for p in self.tokenize(s)]
+        return " ".join(secuencia_normalizada)
+
+    def norm_palabra(self, palabra):
+        palabra = palabra.strip(punctuation)
+        formas = self.fst.apply(palabra)
+        if not formas:
+            return palabra
+        else:
+            return formas[0]
