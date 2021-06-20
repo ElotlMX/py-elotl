29d904ecc4662915176dac02f97d9b6ea762f279
Robert Pugh <robert.pugh408@gmail.com>
2021-06-19 22:12:26 -0700

    print ortografia original, fonemica, y normalizada

--- elotl/nahuatl/fst/fst_ortho_normalizer.py
+++ elotl/nahuatl/fst/fst_ortho_normalizer.py
@@ -1,32 +1,32 @@
 import argparse
-from typing import Callable
 from pathlib import Path
 from string import punctuation
 import attapply
 
+path_to_orig_fon = Path("att", "orig-fon.att")
+orig_fon_fst = attapply.ATTFST(path_to_orig_fon)
 
-class Normalizador(object):
-    def __init__(self, ortografia, tokenize: Callable = lambda s: s.split()):
-        path_to_orig_fon = Path("att", "orig-fon.att")
-        path_to_hfst = Path("att", f"fon-{ortografia}.att")
-        self.tokenize = tokenize
-        self.fon_fst = attapply.ATTFST(path_to_orig_fon)
-        self.fst = attapply.ATTFST(path_to_hfst)
 
-    def norm(self, s):
-        secuencia_normalizada = [self.norm_palabra(p) for p in self.tokenize(s.lower())]
-        return " ".join(secuencia_normalizada)
+class TextoNormalizado:
+    def __init__(self, s, ortografia_normalizada="sep-u-j"):
+        self.norm_fst = attapply.ATTFST(Path("att", f"fon-{ortografia_normalizada}.att"))
+        self.original = s
+        self.fonemica = self._convertir(self.original, orig_fon_fst)
+        self.normalizada = self._convertir(self.fonemico, self.norm_fst)
 
-    def norm_palabra(self, palabra):
-        palabra = palabra.strip(punctuation)
-        formas_fonemicas = list(self.fon_fst.apply(palabra))
-        if not formas_fonemicas:
-            return palabra
-        formas = list(self.fst.apply(formas_fonemicas[-1][0]))
-        if not formas:
-            return palabra
-        else:
-            return formas[-1][0]
+    def _convertir(self, s, fst):
+        tokens_convertidos = []
+        for token in s.split():
+            token = token.strip(punctuation).lower()
+            formas = list(fst.apply(token))
+            if not formas:
+                tokens_convertidos.append(token)
+            else:
+                tokens_convertidos.append(formas[-1][0])
+        return " ".join(tokens_convertidos)
+
+    def __repr__(self):
+        return "Original: {}\nFonemica: {}\nNormalizada:{}".format(self.original, self.fonemico, self.normalizado)
 
 
 if __name__ == "__main__":
@@ -35,6 +35,5 @@ if __name__ == "__main__":
     argparser.add_argument("--ortografia_preferida", "-ort", choices=["sep-u-j", "sep-w-h", "ack"], default="sep-u-j")
 
     args = argparser.parse_args()
-
-    normalizador = Normalizador(args.ortografia_preferida)
-    print(normalizador.norm(args.texto))
+    texto = TextoNormalizado(args.texto, args.ortografia_preferida)
+    print(texto)
