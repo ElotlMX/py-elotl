d42ef8fcd0c38c5b41e8b09615fafe0510d872fb
Robert Pugh <robert.pugh408@gmail.com>
2021-07-10 00:15:39 -0700

    update normalization script

--- elotl/nahuatl/fst/orthography.py
+++ elotl/nahuatl/fst/orthography.py
@@ -8,37 +8,47 @@ ORIG_FON_FST = attapply.ATTFST(path_to_orig_fon)
 
 
 class Normalizador(object):
-    def __init__(self, input_ort=None, output_ort="sep-u-j"):
+    def __init__(self, input_ort=None, output_ort="sep", overrides=None):
         self.norm_fst = attapply.ATTFST(Path("att", f"fon-{output_ort}.att"))
+        self.overrides = overrides if overrides is not None else {}
 
-    def _convert_word(self, original_word):
-        w = original_word.lower()
-        fon = list(ORIG_FON_FST.apply(w))
-        if not fon:
-            raise ValueError("Unable to convert word '{}' to phonemes".format(w))
+    def _convert(self, w, fst):
+        w = w.lower()
+        forms = list(fst.apply(w))
+        if forms:
+            return forms[-1][0]
         else:
-            fon = fon[-1][0]
-            out_token = list(self.norm_fst.apply(fon))
-            if not out_token:
-                raise ValueError("Unable to convert phoneme sequence '{}' to normalized orthgraphy.".format(fon))
-            else:
-                return fon, out_token[-1][0]
+            raise ValueError("Unable to convert word '{}'".format(w, fst))
+
+    def _normalize_word(self, original_word):
+        w = original_word.lower()
+        fon = self._convert(w, ORIG_FON_FST)
+        normed = self._convert(fon, self.norm_fst)
+        return fon, normed
+
+    def _tokenize(self, sent):
+        return sent.split()
+
+    def g2p(self, s, overrides=None):
+        overrides = overrides if overrides is not None else {}
+        return " ".join([self._convert(w, ORIG_FON_FST) if not w in overrides else overrides[w] 
+                         for w in self._tokenize(s)])
 
     def normalize(self, s):
-        orig, fon, norm = [], [], []
-        for t in s.split():
-            t_fon, t_norm = self._convert_word(t)
-            orig.append(t)
-            fon.append(t_fon)
+        norm = []
+        for t in self._tokenize(s):
+            if t in self.overrides:
+                norm.append(t)
+            _, t_norm = self._normalize_word(t)
             norm.append(t_norm)
 
-        return " ".join(orig), " ".join(fon), " ".join(norm)
+        return " ".join(norm)
 
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument("texto")
-    argparser.add_argument("--ortografia_preferida", "-ort", choices=["sep-u-j", "sep-w-h", "ack"], default="sep-u-j")
+    argparser.add_argument("--ortografia_preferida", "-ort", choices=["sep", "ack"], default="sep")
 
     args = argparser.parse_args()
     n = Normalizador(output_ort=args.ortografia_preferida)
