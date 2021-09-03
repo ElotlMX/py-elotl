3608075dc50ac88d75d16859d31be37b3969a6dd
penserbjorne <paul.aguilar.enriquez@hotmail.com>
2021-09-03 00:13:36 -0500

    Published v0.0.1.11 on pypy

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="elotl",
-    version="0.0.1.10",
+    version="0.0.1.11",
     author="Paul Aguilar (@penserbjorne)",
     author_email="paul.aguilar.enriquez@hotmail.com",
     description="Paquete para PLN de lenguas originarias",
@@ -23,7 +23,8 @@ setuptools.setup(
     ],
     python_requires='>=3',
     install_requires=[
-        'importlib_resources'
+        'importlib_resources',
+        'future'
     ],
     include_package_data=True,
     zip_safe=True,
