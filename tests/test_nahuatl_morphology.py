b08ccdd3b82194062f7dd936d23f5c5de8dc7d98
Francis Tyers <ftyers@prompsit.com>
2022-01-19 17:08:21 +0000

    add unit test for nahuatl analyser

---
+++ tests/test_nahuatl_morphology.py
@@ -0,0 +1,37 @@
+import unittest
+from elotl.nahuatl.morphology import Analyser
+from json import JSONEncoder
+import json
+
+
+class TokenEncoder(JSONEncoder):
+	def default(self, o):
+		return o.__dict__
+
+class TestAnalysis(unittest.TestCase):
+	analyser = Analyser()
+
+	def test_analysis_tokenisation(self):
+
+		inp = "Nomama."
+		outp = [{"wordform": "Nomama", "analyses": [[[{"lemma": "mama", "pos": "NOUN", "feats": {"Number[psor]": "Sing", "Person[psor]": "1"}}], 0.0], [[{"lemma": "mama", "pos": "NOUN", "feats": {"Number[psor]": "Sing", "Person[subj]": "3", "Number[subj]": "Sing", "Person[psor]": "1"}}], 1.0]], "pos": "NOUN", "lemma": "mama"}, {"wordform": ".", "analyses": [[[{"lemma": ".", "pos": "PUNCT", "feats": {}}], 0.0]], "pos": "PUNCT", "lemma": "."}]
+		res = self.analyser.analyse(inp, tokenise=True)
+		outpj = json.dumps(res, cls=TokenEncoder)
+		resj = json.dumps(res, cls=TokenEncoder)
+		self.assertEqual(resj, outpj)
+
+	def test_analysis(self):
+
+		inp = ["Nomama","."]
+		outp = [{"wordform": "Nomama", "analyses": [[[{"lemma": "mama", "pos": "NOUN", "feats": {"Number[psor]": "Sing", "Person[psor]": "1"}}], 0.0], [[{"lemma": "mama", "pos": "NOUN", "feats": {"Number[psor]": "Sing", "Person[subj]": "3", "Number[subj]": "Sing", "Person[psor]": "1"}}], 1.0]], "pos": "NOUN", "lemma": "mama"}, {"wordform": ".", "analyses": [[[{"lemma": ".", "pos": "PUNCT", "feats": {}}], 0.0]], "pos": "PUNCT", "lemma": "."}]
+		res = self.analyser.analyse(inp)
+		outpj = json.dumps(res, cls=TokenEncoder)
+		resj = json.dumps(res, cls=TokenEncoder)
+		self.assertEqual(resj, outpj)
+
+
+if __name__ == '__main__':
+	unittest.main()
+
+
+
