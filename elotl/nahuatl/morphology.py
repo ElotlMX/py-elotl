f2e68f863b2f9a4abc983bf94e08e75ffc096375
Francis Tyers <ftyers@prompsit.com>
2022-01-19 00:34:04 +0000

    fix flake8 warnings

--- elotl/nahuatl/morphology.py
+++ elotl/nahuatl/morphology.py
@@ -1,38 +1,38 @@
 # -*- coding: UTF-8 -*-
 
-# Ejemplo de uso:
+"""
+Ejemplo de uso:
 
-#	>>> from elotl.nahuatl.morphology import Analyser
-#	>>> a = Analyser()
-#	>>> res = a.analyse('Huan nomama onechilhuaya, “Amo quen ximati, teh xiyo in escuela.', tokenise=True)
 
-#from __future__ import annotations
-import logging
-import re 
+	>>> from elotl.nahuatl.morphology import Analyser
+	>>> a = Analyser()
+	>>> res = a.analyse('“Amo quen ximati, teh xiyo in escuela.', tokenise=True)
+"""
+
+import re
 from elotl.utils.fst.attapply import ATTFST
 
 try:
-    # For Python >= 3.7
-    import importlib.resources as pkg_resources
+	# For Python >= 3.7
+	import importlib.resources as pkg_resources
 except ImportError:
-    # Try backported to Python < 3.7 `importlib_resources`.
-    import importlib_resources as pkg_resources
+	# Try backported to Python < 3.7 `importlib_resources`.
+	import importlib_resources as pkg_resources
+
 
 class Token(object):
 	"""
-	Each analysis is formed of a weight and a sequence of words, a token may 
-	contain more than one word, a word is a triple of (lemma, pos, [feat=value])
-
-	Parameters
-	----------
-
+	Each analysis is formed of a weight and a sequence of words, a token may
+	contain more than one word, a word is a dictionary of
+		{lemma, pos, [feat=value]}
 	"""
-	def __init__(self, wordform, analyses = []):
+	def __init__(self, wordform, analyses=[]):
 		self.wordform = wordform
 		self.analyses = analyses
 
 	def __repr__(self):
-		return '<Token "{0}" ({1})>'.format(self.wordform,len(self.analyses))
+		return '<Token "{0}" ({1})>'.format(self.wordform, len(self.analyses))
+
 
 class Convertor(object):
 	"""
@@ -42,8 +42,9 @@ class Convertor(object):
 	Parameters
 	----------
 	rule_file: str
-		A path to the location of the TSV file containing the tagset conversion rules.
-		Each rule is composed of 9 columns: 
+		A path to the location of the TSV file containing the tagset
+		conversion rules.
+		Each rule is composed of 9 columns:
 			1. Priority
 			2. Input lemma
 			3. Input POS
@@ -69,10 +70,10 @@ class Convertor(object):
 		tags: str
 			A sequence of tags separated by the pipe symbol, |
 
-		Returns 
+		Returns
 		----------
 		str
-			A sequences of tags encased in less-than and greater-than < > 
+			A sequences of tags encased in less-than and greater-than < >
 		"""
 
 		if tags != '':
@@ -81,15 +82,16 @@ class Convertor(object):
 
 	def _load_conversion_rules(self, fn):
 		"""
-		Loads the conversion rules and scores each rule. Longer rules are scored higher,
-		rules containing lemmas are scored higher.
+		Loads the conversion rules and scores each rule. Longer rules are
+		scored higher, rules containing lemmas are scored higher.
 
 		Parameters
 		----------
 		fn: str
-			A path to the location of the TSV file containing the tagset conversion rules.
+			A path to the location of the TSV file containing the
+			tagset conversion rules.
 
-		Returns 
+		Returns
 		----------
 		dict
 			A dictionary containing a set of symbols used for matching and a list of
@@ -105,23 +107,22 @@ class Convertor(object):
 			score = priority
 			inn = [re.sub('^_$', '', i) for i in row[1:5]]
 			out = [re.sub('^_$', '', i) for i in row[5:]]
-	
+
 			if inn[0] != '': score += 4
 			if inn[1] != '': score += 3
 			if inn[2] != '': score += (2 * len(inn[2].split('|')))
 			if inn[3] != '': score += 1
-	
-			inn = [inn[0], self._convert_tags(inn[1]), self._convert_tags(inn[2]), inn[3]] 
+
+			inn = [inn[0],
+					self._convert_tags(inn[1]), self._convert_tags(inn[2]), inn[3]]
 			rules['sym'].add(inn[1])
 			rules['sym'].add(inn[2])
 			rules['sub'].append((score, inn, out))
-	
+
 		rules['sym'] = list(rules['sym'])
-		rules['sym'].sort(key=lambda x:len(x), reverse=True)
+		rules['sym'].sort(key=lambda x: len(x), reverse=True)
 		rules['sub'].sort()
 		return rules
-
-
 
 	def _convert(self, a):
 		"""
@@ -134,14 +135,14 @@ class Convertor(object):
 			An Apertium-compatible analysis, e.g. <s_sg1>quiza<v><iv><pret>, note
 			that by this point there should be no subwords.
 
-		Returns 
+		Returns
 		----------
 		dict
-			A dictionary containing a lemma, a part-of-speech and a 
+			A dictionary containing a lemma, a part-of-speech and a
 			set of Feature=Value pairs.
 		"""
 
-		analysis = {'lemma':'', 'pos':'', 'feats': set()}
+		analysis = {'lemma': '', 'pos': '', 'feats': set()}
 		tags = [i for i in self.input_patterns.findall(a) if not i == '']
 		msd = set(tags)
 		analysis['lemma'] = re.sub('<[^>]+>', '', a)
@@ -168,30 +169,31 @@ class Convertor(object):
 	def convert(self, analysis_):
 		"""
 		The main function for conversion, takes a full analysis, including possible
-		subwords, e.g. ya<adv>+<s_sg1>quiza<v><iv><pret> and returns a list of 
-		syntactic words. 
+		subwords, e.g. ya<adv>+<s_sg1>quiza<v><iv><pret> and returns a list of
+		syntactic words.
 
 		Parameters
 		----------
 		analysis_: str
 			An Apertium-compatible analysis.
 
-		Returns 
+		Returns
 		----------
 		list
 			A list of the component analyses by syntactic word.
 		"""
 
-		analysis = []	
+		analysis = []
 		subwords = analysis_.split('+')
 		for word in subwords:
 			analysis.append(self._convert(word))
 		return analysis
 
+
 class Analyser(object):
 	"""
-	Class for returning morphological analyses in a Python-friendly format with UD style
-	POS tags and Feature=Value pairs.
+	Class for returning morphological analyses in a Python-friendly format
+	with UD-style POS tags and Feature=Value pairs.
 
 	Parameters
 	----------
@@ -216,48 +218,51 @@ class Analyser(object):
 
 	def _tokenise(self, text):
 		"""
-		Internal backoff tokenisation function. 
+		Internal backoff tokenisation function.
 
 		Parameters
 		----------
 		text: str
 			The text to be tokenised.
 
-		Returns 
+		Returns
 		----------
 		list
 			List of space separated tokens.
-		
+
 		"""
 
-		tokens = re.sub('([^a-zA-Z]+)', ' \g<1> ', text)
-		return [token.strip() for token in tokens.split(' ') if not token.strip() == '']
+		tokens = re.sub(r'([^a-zA-Z]+)', r' \g<1> ', text)
+		return [token.strip()
+			for token in tokens.split(' ')
+				if not token.strip() == '']
 
 	def _convert_analysis(self, analysis):
 		"""
-		Takes an analysis returned by the transducer and converts it to a more Python-friendly
-		format. 
+		Takes an analysis returned by the transducer and converts it
+		to a more Python-friendly format.
 
 		Parameters
 		----------
 		analysis: str
 			An analysis for a surface token, e.g. ya<adv>+<s_sg1>quiza<v><iv><pret>
 
-		Returns 
+		Returns
 		----------
 		list
 			A list of syntactic words with their analyses in UD format
-			[{'lemma': 'ya', 'pos': 'ADV', 'feats': set()}, 
+			[{'lemma': 'ya', 'pos': 'ADV', 'feats': set()},
 				{'lemma': 'quiza', 'pos': 'VERB', 'feats': {
-				'Number[subj]=Sing', 'Person[subj]=1', 'Tense=Past', 'VerbForm=Fin', 'Subcat=Intr'}}]
+				'Number[subj]=Sing', 'Person[subj]=1', 'Tense=Past', i
+					'VerbForm=Fin', 'Subcat=Intr'}}]
 		"""
 
 		analyses = self.convertor.convert(analysis)
 		return analyses
 
-	def _analyse_token(self,token, alternative=''):
+	def _analyse_token(self, token, alternative=''):
 		"""
-		Function that takes a token and returns a list of analyses with their 
+		Function that takes a token and returns a list of analyses with their
 		weights as assigned by the transducer.
 
 		Parameters
@@ -268,7 +273,7 @@ class Analyser(object):
 		alternative: str
 			An alternative form, e.g. a lowercased or normalised form
 
-		Returns 
+		Returns
 		----------
 		list
 			A list of the possible analyses with weights
@@ -276,17 +281,16 @@ class Analyser(object):
 		analyses = list(self.analyser.apply(token))
 		if len(analyses) == 0 and alternative:
 			analyses = list(self.analyser.apply(alternative))
-				
+
 		converted = []
 		for (analysis, weight) in analyses:
 			converted.append((self._convert_analysis(analysis), weight))
 		return converted
 
-
-	def analyse(self, text, tokenise = False):
+	def analyse(self, text, tokenise=False):
 		"""
-		An analyse function that can take either a string with a tokeniser, 
-		a pre-tokenised list or a pre-tokenised string. If it is passed a 
+		An analyse function that can take either a string with a tokeniser,
+		a pre-tokenised list or a pre-tokenised string. If it is passed a
 		tokeniser the tokeniser is first run and then the list is analysed,
 		if passed a list, the list is analysed, if passed a pre-tokenised
 		list then the list is split on space and then analysed.
@@ -297,10 +301,10 @@ class Analyser(object):
 		text: str or list
 			Input text/sentence. It can be either a string or a list. It
 			should be pre-tokenised, alternatively see below.
-		
+
 		tokenise: bool
 			Should tokenisation be performed on the input prior to analysis?
-			
+
 		Returns
 		----------
 		list
@@ -319,13 +323,13 @@ class Analyser(object):
 
 		tokens = []
 		wordforms = []
-		if tokenise == False:
+		if not tokenise:
 			if type(text) == str:
 				wordforms = text.split(' ')
 			elif type(text) == list:
 				wordforms = text
 		else:
-			wordforms = self.tokenise(text)	
+			wordforms = self.tokenise(text)
 
 		for wordform in wordforms:
 			analyses = self._analyse_token(wordform, wordform.lower())
@@ -334,7 +338,7 @@ class Analyser(object):
 
 		return tokens
 
-	def analyze(self, text, tokenize = False):
+	def analyze(self, text, tokenize=False):
 		"""
 		Convenience alias of analyse() with alternative spelling.
 
@@ -343,10 +347,10 @@ class Analyser(object):
 		text: str or list
 			Input text/sentence. It can be either a string or a list. It
 			should be pre-tokenised, alternatively see below.
-		
+
 		tokenize: bool
 			Should tokenisation be performed on the input prior to analysis?
-			
+
 		Returns
 		----------
 		list
